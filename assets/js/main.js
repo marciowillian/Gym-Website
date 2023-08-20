1063279608f3f1d55e3137fa7354a01d918f117e
Marcio Willian <willian_mw.cc@hotmail.com>
2023-08-20 16:20:07 -0300

    Innitial commit

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+/**
+ * Show menu
+ */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/**
+ * Hidden menu
+ */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+        //When we click on each nav__link, we remove the show-menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+
+/*=============== CALCULATE JS ===============*/
+
+
+/*=============== EMAIL JS ===============*/
