80830c9dbd7037dafd3b1811677551ec196dcb7c
Marcio Willian <willian_mw.cc@hotmail.com>
2024-01-17 14:52:47 -0300

    Implementing responsiveness for all main screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,21 +43,36 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-//TO DO
-// const sections = document.querySelectorAll('section[id')
+const sections = document.querySelectorAll('section[id')
 
-// const scrollActive = () => {
-//     const scrollY = window.pageYOffset
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
 
-//     sections.forEach(current => {
-//         const sectionHeight = current.offsetHeight,
-//         sectionTop = current.offset - 58,
-//         sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId ])
-//     })
-// }
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+        //When the scrollis higher than 350 viewport height, add show-scroll class to the a tag with the scrollup
+    this.scrollY >= 350 ?
+        scrollUp.classList.add('show-scroll') :
+        scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
